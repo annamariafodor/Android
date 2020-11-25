d9272b0162de0b5912c4518c87698675f3056df7
annamariafodor <annamariafodor98@gmail.com>
2020-11-25 23:47:11 +0200

    created the layout

---
+++ app/src/main/java/com/example/wheretoeat/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wheretoeat.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wheretoeat.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
