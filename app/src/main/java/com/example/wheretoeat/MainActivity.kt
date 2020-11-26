15159edaca666bf3b9c766540c5927e887603827
annamariafodor <annamariafodor98@gmail.com>
2020-11-26 02:48:24 +0200

    added bottom navigation

--- app/src/main/java/com/example/wheretoeat/MainActivity.kt
+++ app/src/main/java/com/example/wheretoeat/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.wheretoeat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.wheretoeat.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavigation()
     }
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+    fun setUpNavigation() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment?
+        NavigationUI.setupWithNavController(
+            bottomNavigationView,
+            navHostFragment!!.navController
+        )
+    }
+
 }
